6900afd712a1aa5d9f0e31ed01140211e1c46a2d
Devin Pinkston <dpinkston@HW13466.fios-router.home>
2016-10-12 07:45:53 -0400

    initial commit

---
+++ nifi-accumulo-service-api/src/main/java/org/apache/nifi/accumulo/AccumuloClientService.java
@@ -0,0 +1,123 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.accumulo;
+
+import org.apache.nifi.accumulo.put.PutFlowFile;
+import org.apache.nifi.accumulo.put.PutMutation;
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.controller.ControllerService;
+import org.apache.nifi.processor.util.StandardValidators;
+
+import java.io.IOException;
+import java.util.Collection;
+
+@Tags({"Accumulo, Client"})
+@CapabilityDescription("A controller service for accessing an Accumulo connector")
+public interface AccumuloClientService extends ControllerService {
+
+
+    public static final PropertyDescriptor ZOOKEEPER_QUORUM = new PropertyDescriptor.Builder()
+            .name("ZooKeeper Quorum")
+            .description("Comma-separated list of ZooKeeper hosts for Accumulo. Required if Hadoop Configuration Files are not provided.")
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .required(true)
+            .build();
+
+    public static final PropertyDescriptor INSTANCE_NAME = new PropertyDescriptor.Builder()
+            .name("Instance Name")
+            .description("All clients must first identify the Accumulo instance to which they will be communicating.")
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .required(true)
+            .build();
+
+    public static final PropertyDescriptor USER = new PropertyDescriptor.Builder()
+            .name("Username")
+            .description("The user name for connecting to Accumulo")
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .required(true)
+            .build();
+
+    public static final PropertyDescriptor PASSWORD = new PropertyDescriptor.Builder()
+            .name("Password")
+            .description("The Password for connecting to Accumulo")
+            .sensitive(true)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .required(true)
+            .build();
+
+    /**
+     * Puts a batch of mutations to the given table.
+     *
+     * @param tableName the name of an Accumulo table
+     * @param mutations      a list of put mutations for the given table
+     * @throws IOException thrown when there are communication errors with Accumulo
+     */
+    void put(String tableName, Collection<PutFlowFile> mutations) throws IOException;
+
+    /**
+     * Puts the given row to HBase with the provided columns.
+     *
+     * @param tableName the name of an HBase table
+     * @param rowId     the id of the row to put
+     * @param mutations the columns of the row to put
+     * @throws IOException thrown when there are communication errors with HBase
+     */
+    void put(String tableName, String rowId, Collection<PutMutation> mutations) throws IOException;
+
+    /**
+     * Converts the given boolean to it's byte representation.
+     *
+     * @param b a boolean
+     * @return the boolean represented as bytes
+     */
+    byte[] toBytes(boolean b);
+
+    /**
+     * Converts the given long to it's byte representation.
+     *
+     * @param l a long
+     * @return the long represented as bytes
+     */
+    byte[] toBytes(long l);
+
+    /**
+     * Converts the given double to it's byte representation.
+     *
+     * @param d a double
+     * @return the double represented as bytes
+     */
+    byte[] toBytes(double d);
+
+    /**
+     * Converts the given string to it's byte representation.
+     *
+     * @param s a string
+     * @return the string represented as bytes
+     */
+    byte[] toBytes(String s);
+
+    /**
+     * Converts the given binary formatted string to a byte representation
+     * @param s a binary encoded string
+     * @return the string represented as bytes
+     */
+    byte[] toBytesBinary(String s);
+
+
+}
---
+++ nifi-accumulo_1_7_2-client-service-bundle/src/main/java/org/apache/nifi/accumulo/Accumulo_1_7_2_ConnectorService.java
@@ -0,0 +1,227 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.accumulo;
+
+import org.apache.accumulo.core.client.*;
+import org.apache.accumulo.core.client.security.tokens.PasswordToken;
+import org.apache.accumulo.core.data.Mutation;
+import org.apache.accumulo.core.data.Value;
+import org.apache.accumulo.core.security.ColumnVisibility;
+import org.apache.hadoop.io.Text;
+import org.apache.nifi.accumulo.put.PutFlowFile;
+import org.apache.nifi.accumulo.put.PutMutation;
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.annotation.lifecycle.OnDisabled;
+import org.apache.nifi.annotation.lifecycle.OnEnabled;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.components.ValidationContext;
+import org.apache.nifi.components.ValidationResult;
+import org.apache.nifi.controller.AbstractControllerService;
+import org.apache.nifi.controller.ConfigurationContext;
+import org.apache.nifi.controller.ControllerServiceInitializationContext;
+import org.apache.nifi.processor.util.StandardValidators;
+import org.apache.nifi.reporting.InitializationException;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+@Tags({"Accumulo, Connection, Instance"})
+@CapabilityDescription("Provides Accumulo connector to a given instance")
+public class Accumulo_1_7_2_ConnectorService extends AbstractControllerService implements AccumuloClientService {
+
+    private Connector connector;
+    private List<PropertyDescriptor> properties;
+
+    @Override
+    protected void init(ControllerServiceInitializationContext config) throws InitializationException {
+
+        List<PropertyDescriptor> props = new ArrayList<>();
+        props.add(ZOOKEEPER_QUORUM);
+        props.add(INSTANCE_NAME);
+        props.add(USER);
+        props.add(PASSWORD);
+        this.properties = Collections.unmodifiableList(props);
+
+    }
+
+    @Override
+    protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        return properties;
+    }
+
+    @Override
+    protected PropertyDescriptor getSupportedDynamicPropertyDescriptor(String propertyDescriptorName) {
+        return new PropertyDescriptor.Builder()
+                .description("Specifies the value for '" + propertyDescriptorName + "' in the HBase configuration.")
+                .name(propertyDescriptorName)
+                .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+                .dynamic(true)
+                .build();
+    }
+
+    @Override
+    protected Collection<ValidationResult> customValidate(ValidationContext validationContext) {
+        boolean zkQuorumProvided = validationContext.getProperty(ZOOKEEPER_QUORUM).isSet();
+        boolean instanceNamerovided = validationContext.getProperty(INSTANCE_NAME).isSet();
+        boolean userProvided = validationContext.getProperty(USER).isSet();
+        boolean passwordProvided = validationContext.getProperty(PASSWORD).isSet();
+
+        final List<ValidationResult> problems = new ArrayList<>();
+
+        if (!zkQuorumProvided && (!instanceNamerovided || !userProvided || !passwordProvided)) {
+            problems.add(new ValidationResult.Builder()
+                    .valid(false)
+                    .subject(this.getClass().getSimpleName())
+                    .explanation("ZooKeeper Quorum, Instance Name, Accumulo User, and Accumulo Password are required " +
+                            "to connect")
+                    .build());
+        }
+        return problems;
+    }
+
+
+    @OnEnabled
+    public void onEnabled(final ConfigurationContext context) throws InitializationException, IOException, InterruptedException {
+
+        String instanceName = context.getProperty(INSTANCE_NAME).getValue();
+        String zkQuorum = context.getProperty(ZOOKEEPER_QUORUM).getValue();
+        String accumuloUser = context.getProperty(USER).getValue();
+        String accumuloPassword = context.getProperty(PASSWORD).getValue();
+
+        Instance inst = new ZooKeeperInstance(instanceName, zkQuorum);
+
+        try {
+            this.connector = inst.getConnector(accumuloUser, new PasswordToken(accumuloPassword));
+        } catch (AccumuloException e) {
+            e.printStackTrace();
+        } catch (AccumuloSecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @OnDisabled
+    public void shutdown() {
+        if (connector != null) {
+            connector = null;
+        }
+    }
+
+    @Override
+    public void put(final String tableName, final Collection<PutFlowFile> puts) throws IOException {
+
+        getLogger().info("Created a Put for PutFlowFile");
+        BatchWriter writer = batchWriter(10000000L, tableName);
+
+        getLogger().info("puts length: " + puts.size());
+
+        for (final PutFlowFile putFlowFile : puts) {
+            for (final PutMutation column : putFlowFile.getColumns()) {
+                Mutation mutation = new Mutation(new Text(putFlowFile.getRow()));
+                mutation.put(new Text(column.getColumnFamily()), new Text(column.getColumnQualifier()),
+                        new ColumnVisibility(column.getColumnVisibilty()), new Value(column.getBuffer().getBytes()));
+                try {
+                    writer.addMutation(mutation);
+                } catch (MutationsRejectedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        try {
+            getLogger().info("closing batchwriter in putflowfile");
+            closeWriter(writer);
+        } catch (MutationsRejectedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void put(final String tableName, final String rowId, final Collection<PutMutation> columns) throws IOException {
+
+        getLogger().info("Created a put for PutMutation");
+        BatchWriter writer = batchWriter(10000000L, tableName);
+
+        Mutation mutation = new Mutation(new Text(rowId));
+        for (final PutMutation column : columns) {
+            mutation.put(new Text(column.getColumnFamily()), new Text(column.getColumnQualifier()),
+                    new ColumnVisibility(column.getColumnVisibilty()), new Value(column.getBuffer().getBytes()));
+        }
+
+        try {
+            getLogger().info("writing mutation: " + mutation.getRow().toString());
+            writer.addMutation(mutation);
+        } catch (MutationsRejectedException e) {
+            e.printStackTrace();
+        }
+        try {
+            getLogger().info("closing batchwriter in putmutation");
+            closeWriter(writer);
+        } catch (MutationsRejectedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public BatchWriter batchWriter(long maxMemory, String tableName) {
+
+        BatchWriterConfig bwConfig = new BatchWriterConfig();
+        bwConfig.setMaxMemory(maxMemory);
+        BatchWriter batchWriter = null;
+
+        try {
+            getLogger().info("Creating Accumulo Batchwriter");
+            batchWriter = connector.createBatchWriter(tableName, bwConfig);
+        } catch (TableNotFoundException e) {
+            e.printStackTrace();
+        }
+        return batchWriter;
+    }
+
+    public void closeWriter(BatchWriter bw) throws MutationsRejectedException {
+        bw.close();
+    }
+
+    @Override
+    public byte[] toBytes(boolean b) {
+        byte[] vOut = new byte[]{(byte) (b ? 1 : 0)};
+        return vOut;
+    }
+
+    @Override
+    public byte[] toBytes(long l) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(l);
+        return buffer.array();
+    }
+
+    @Override
+    public byte[] toBytes(double d) {
+        byte[] bytes = ByteBuffer.allocate(8).putDouble(d).array();
+        return bytes;
+    }
+
+    @Override
+    public byte[] toBytes(String s) {
+        return s.getBytes(StandardCharsets.UTF_8);
+    }
+
+    @Override
+    public byte[] toBytesBinary(String s) {
+        return s.getBytes(StandardCharsets.UTF_8);
+    }
+}
